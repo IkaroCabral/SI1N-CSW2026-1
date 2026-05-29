ec553d48e970926cf7bd04f998f1a01b13bd2ea4
Ikaro Henique Cravo Cabral Coutinho <henriqueikaro603@gmail.com>
2026-05-28 21:57:48 -0300

    Add files via upload

---
+++ Calculadora 2/javascript.js
@@ -0,0 +1,17 @@
+function calcularidade() {
+    
+    var diaDigitado = document.getElementById('inputDia').value;
+    var mesDigitado = document.getElementById('inputMes').value;
+    var anoDigitado = document.getElementById('inputAno').value;
+    var hoje = new Date() 
+    var anohoje = new Date().getFullYear();
+    var idade = anohoje - anoDigitado;
+    var aniversario = new date (anohoje = "/" + mesDigitado + "/" + diaDigitado);
+      document.getElementById('resultado').innerHTML = "Sua idade é: " + idade + " anos.";
+
+      if (hoje > aniversario)
+    
+     idade --
+
+     select
+}
